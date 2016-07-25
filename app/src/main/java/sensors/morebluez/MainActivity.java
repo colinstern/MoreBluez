9cae980637508c1028842c748425a525eb57845c
cost <cstern19@gmail.com>
2016-07-25 14:24:28 -0400

    Removed default UI and replaced it with a simple TextView.

--- app/src/main/java/sensors/morebluez/MainActivity.java
+++ app/src/main/java/sensors/morebluez/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,23 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         if (savedInstanceState == null) {
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
             BluetoothFragment fragment = new BluetoothFragment();
             transaction.replace(R.id.sample_content_fragment, fragment);
             transaction.commit();
+        }
+
+        TextView textbox = (TextView) findViewById(R.id.textbox);
+        int global = 0;
+        for (int k = 2; k < 5000; k++) {
+            boolean isPrime = true;
+            for (int j = 2; j < k; j++)
+                if (k % j == 0) { isPrime = false; break; }
+            if (!isPrime) continue;
+            global ++;
+            textbox.append(String.format("\nPrime #%d is %,d", global, k));
         }
     }
 
